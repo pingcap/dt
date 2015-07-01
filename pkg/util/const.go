bb9fdbbcb45a2a1506608ee010dd7ad86dab97f6
zimulala <zimu_xia@126.com>
2015-07-01 20:13:04 +0800

    add some constants

---
+++ pkg/util/const.go
@@ -0,0 +1,20 @@
+package util
+
+// instance_agent api url
+const (
+	UrlStartInstance         = "api/start_instance"
+	UrlRestartInstance       = "api/restart_instance"
+	UrlPauseInstance         = "api/pause_instance"
+	UrlContinuePauseInstance = "api/continuepause_instance"
+	UrlStopInstance          = "api/stop_instance"
+	UrlDropPortInstance      = "api/dropport_instance"
+	UrlContinueInstance      = "api/continueport_instance"
+	UrlBackupInstanceData    = "api/backupdata_instance"
+	UrlCleanUpInstanceData   = "api/cleanUpdata_instance"
+	UrlShutdown              = "api/Shutdown"
+)
+
+// ctroller api url
+const (
+	UrlRegisterAgent = "api/register_agent"
+)
