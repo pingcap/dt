7e49a2680fb1f2ef3882b8e5c11dcbc45b4503a2
zimulala <zimu_xia@126.com>
2015-07-03 21:23:58 +0800

    implement the function of apiRegisterAgent

--- pkg/controller/api.go
+++ pkg/controller/api.go
@@ -1,22 +1,38 @@
 package controller
 
 import (
+	"errors"
 	"github.com/gorilla/mux"
+	"io"
 	"net/http"
 
+	"github.com/ngaut/log"
 	"github.com/pingcap/dt/pkg/util"
 )
 
+var ErrRegisterArgsInvalid = errors.New("invalid register args")
+
 func runHttpServer(addr string, ctrl *Controller) {
+	log.Debug("start: runHttpServer")
 	m := mux.NewRouter()
 
-	m.HandleFunc(util.UrlRegisterAgent, ctrl.apiRegisterAgent).Methods("POST", "PUT")
+	m.HandleFunc("/"+util.ActionRegisterAgent, ctrl.apiRegisterAgent).Methods("POST", "PUT")
 
 	http.Handle("/", m)
 	http.ListenAndServe(addr, nil)
 }
 
-// TODO: implement
 func (ctrl *Controller) apiRegisterAgent(w http.ResponseWriter, r *http.Request) {
-	panic("apiRegisterAgent hasn't implemented")
+	log.Debug("start: apiRegisterAgent")
+	agentAddr := r.FormValue("addr")
+
+	if agentAddr == "" {
+		w.WriteHeader(http.StatusInternalServerError)
+		io.WriteString(w, ErrRegisterArgsInvalid.Error())
+		return
+	}
+	log.Info("apiRegisterAgent, info:", agentAddr)
+	ctrl.agentInfoCh <- agentAddr
+
+	w.WriteHeader(http.StatusOK)
 }
