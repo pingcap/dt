8f651baeebd8a1423f2f383b04d3deb4ad40f019
zimulala <zimu_xia@126.com>
2015-07-03 21:35:12 +0800

    modify constants

--- pkg/util/const.go
+++ pkg/util/const.go
@@ -2,19 +2,30 @@ package util
 
 // instance_agent api url
 const (
-	UrlStartInstance         = "api/start_instance"
-	UrlRestartInstance       = "api/restart_instance"
-	UrlPauseInstance         = "api/pause_instance"
-	UrlContinuePauseInstance = "api/continuepause_instance"
-	UrlStopInstance          = "api/stop_instance"
-	UrlDropPortInstance      = "api/dropport_instance"
-	UrlContinueInstance      = "api/continueport_instance"
-	UrlBackupInstanceData    = "api/backupdata_instance"
-	UrlCleanUpInstanceData   = "api/cleanUpdata_instance"
-	UrlShutdown              = "api/Shutdown"
+	ActionStartInstance       = "api/start_instance"
+	ActionRestartInstance     = "api/restart_instance"
+	ActionPauseInstance       = "api/pause_instance"
+	ActionContinueInstance    = "api/continue_instance"
+	ActionStopInstance        = "api/stop_instance"
+	ActionDropPortInstance    = "api/dropport_instance"
+	ActionRecoverPortInstance = "api/Recoverport_instance"
+	ActionBackupInstanceData  = "api/backupdata_instance"
+	ActionCleanUpInstanceData = "api/cleanUpdata_instance"
+	ActionShutdown            = "api/Shutdown"
 )
 
 // ctroller api url
 const (
-	UrlRegisterAgent = "api/register_agent"
+	ActionRegisterAgent = "api/register_agent"
+)
+
+// ctroller test cmd name
+const (
+	TestCmdStart       = "start"
+	TestCmdRestart     = "restart"
+	TestCmdPause       = "pause"
+	TestCmdContinue    = "continue"
+	TestCmdStop        = "stop"
+	TestCmdDropPort    = "dropport"
+	TestCmdRecoverPort = "recoverport"
 )
