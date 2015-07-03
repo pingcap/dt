e7997c13ce29ab021f53640bdf7239e0f366f7bb
zimulala <zimu_xia@126.com>
2015-07-03 21:29:18 +0800

    modify the apiStartInstance function

--- pkg/agent/api.go
+++ pkg/agent/api.go
@@ -11,16 +11,16 @@ import (
 func runHttpServer(a *Agent) error {
 	m := mux.NewRouter()
 
-	m.HandleFunc(util.UrlStartInstance, a.apiStartInstance).Methods("Post", "Put")
-	m.HandleFunc(util.UrlRestartInstance, a.apiRestartInstance).Methods("Post", "Put")
-	m.HandleFunc(util.UrlPauseInstance, a.apiPauseInstance).Methods("Post", "Put")
-	m.HandleFunc(util.UrlContinuePauseInstance, a.apiContinuePauseInstance).Methods("Post", "Put")
-	m.HandleFunc(util.UrlStopInstance, a.apiStopInstance).Methods("Post", "Put")
-	m.HandleFunc(util.UrlDropPortInstance, a.apiDropPortInstance).Methods("Post", "Put")
-	m.HandleFunc(util.UrlContinueInstance, a.apiContinuePortInstance).Methods("Post", "Put")
-	m.HandleFunc(util.UrlBackupInstanceData, a.apiBackupInstanceData).Methods("Post", "Put")
-	m.HandleFunc(util.UrlCleanUpInstanceData, a.apiCleanUpInstanceData).Methods("Post", "Put")
-	m.HandleFunc(util.UrlShutdown, a.apiShutdown).Methods("Post", "Put")
+	m.HandleFunc("/"+util.ActionStartInstance, a.apiStartInstance).Methods("Post", "Put")
+	m.HandleFunc("/"+util.ActionRestartInstance, a.apiRestartInstance).Methods("Post", "Put")
+	m.HandleFunc("/"+util.ActionPauseInstance, a.apiPauseInstance).Methods("Post", "Put")
+	m.HandleFunc("/"+util.ActionContinueInstance, a.apiContinueInstance).Methods("Post", "Put")
+	m.HandleFunc("/"+util.ActionStopInstance, a.apiStopInstance).Methods("Post", "Put")
+	m.HandleFunc("/"+util.ActionDropPortInstance, a.apiDropPortInstance).Methods("Post", "Put")
+	m.HandleFunc("/"+util.ActionRecoverPortInstance, a.apiRecoverPortInstance).Methods("Post", "Put")
+	m.HandleFunc("/"+util.ActionBackupInstanceData, a.apiBackupInstanceData).Methods("Post", "Put")
+	m.HandleFunc("/"+util.ActionCleanUpInstanceData, a.apiCleanUpInstanceData).Methods("Post", "Put")
+	m.HandleFunc("/"+util.ActionShutdown, a.apiShutdown).Methods("Post", "Put")
 
 	http.Handle("/", m)
 
@@ -42,12 +42,12 @@ func (a *Agent) apiStartInstance(w http.ResponseWriter, r *http.Request) {
 }
 
 // TODO: implement
-func (a *Agent) apiRestartInstance(w http.ResponseWriter, r *http.Request)       {}
-func (a *Agent) apiStopInstance(w http.ResponseWriter, r *http.Request)          {}
-func (a *Agent) apiPauseInstance(w http.ResponseWriter, r *http.Request)         {}
-func (a *Agent) apiContinuePauseInstance(w http.ResponseWriter, r *http.Request) {}
-func (a *Agent) apiBackupInstanceData(w http.ResponseWriter, r *http.Request)    {}
-func (a *Agent) apiCleanUpInstanceData(w http.ResponseWriter, r *http.Request)   {}
-func (a *Agent) apiShutdown(w http.ResponseWriter, r *http.Request)              {}
-func (a *Agent) apiDropPortInstance(w http.ResponseWriter, r *http.Request)      {}
-func (a *Agent) apiContinuePortInstance(w http.ResponseWriter, r *http.Request)  {}
+func (a *Agent) apiRestartInstance(w http.ResponseWriter, r *http.Request)     {}
+func (a *Agent) apiStopInstance(w http.ResponseWriter, r *http.Request)        {}
+func (a *Agent) apiPauseInstance(w http.ResponseWriter, r *http.Request)       {}
+func (a *Agent) apiContinueInstance(w http.ResponseWriter, r *http.Request)    {}
+func (a *Agent) apiBackupInstanceData(w http.ResponseWriter, r *http.Request)  {}
+func (a *Agent) apiCleanUpInstanceData(w http.ResponseWriter, r *http.Request) {}
+func (a *Agent) apiShutdown(w http.ResponseWriter, r *http.Request)            {}
+func (a *Agent) apiDropPortInstance(w http.ResponseWriter, r *http.Request)    {}
+func (a *Agent) apiRecoverPortInstance(w http.ResponseWriter, r *http.Request) {}
