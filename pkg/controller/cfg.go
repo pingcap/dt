82f083d8159dd2f77e14148fa1d2737c6042aa64
zimulala <zimu_xia@126.com>
2015-07-04 19:07:29 +0800

    modify structure name

--- pkg/controller/cfg.go
+++ pkg/controller/cfg.go
@@ -1,26 +1,18 @@
-package util
+package controller
 
 import (
 	"github.com/BurntSushi/toml"
 	"github.com/ngaut/log"
 )
 
-const (
-	CmdStartInstance = "start"
-)
-
 //controller config
 type CtrlCfg struct {
 	Title         string
-	Attr          BaseAttr                `toml:"baseAttr"`
-	InstanceInfos map[string]InstanceInfo `toml:"instanceInfo"`
-	Cmds          []TestCmd               `toml:"testCmd"`
-}
-
-type BaseAttr struct {
 	Addr          string
-	DataDir       string
-	InstanceCount int
+	DataDir       string                  `toml:"data_dir"`
+	InstanceCount int                     `toml:"instance_count"`
+	InstanceInfos map[string]InstanceInfo `toml:"instance_info"`
+	Cmds          []TestCmd               `toml:"test_cmd"`
 }
 
 type InstanceInfo struct {
@@ -35,28 +27,8 @@ type TestCmd struct {
 	Instances []string
 }
 
-//instance_agent config
-type AgentCfg struct {
-	Attr AgentAttr
-}
-
-type AgentAttr struct {
-	Ip       string
-	Port     string
-	CtrlAddr string
-	DataDir  string
-}
-
-func GetCtrlCfg(file string) (cfg *CtrlCfg, err error) {
+func GetCfg(file string) (cfg *CtrlCfg, err error) {
 	log.Debug("start: getCtrlCfg")
-	_, err = toml.DecodeFile(file, &cfg)
-	log.Info(cfg)
-
-	return
-}
-
-func GetAgentCfg(file string) (cfg *AgentCfg, err error) {
-	log.Debug("start: getAgentCfg")
 	_, err = toml.DecodeFile(file, &cfg)
 	log.Info(cfg)
 
