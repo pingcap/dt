859984a0ff8b7d3ed65d173c45e46b38bccbb8c2
zimulala <zimu_xia@126.com>
2015-07-04 19:11:37 +0800

    modify initialization

--- cmd/main.go
+++ cmd/main.go
@@ -5,40 +5,45 @@ import (
 	"runtime"
 
 	"github.com/ngaut/log"
+	"github.com/pingcap/dt/pkg/agent"
 	ctrl "github.com/pingcap/dt/pkg/controller"
-	agent "github.com/pingcap/dt/pkg/instance_agent"
-	"github.com/pingcap/dt/pkg/util"
 )
 
 var (
-	role    = flag.String("role", "instance_agent", "start the specified process")
+	role    = flag.String("role", "agent", "start the specified process: controller, agent [default: agent]")
 	cfgPath = flag.String("cfg", "cmd/cfg.toml", "configure file name")
-	level   = flag.String("loglevel", "debug", "set log level: info, warn, error, debug [default: info]")
+	level   = flag.String("loglevel", "debug", "set log level: info, warn, error, debug [default: debug]")
 )
+
+type Server interface {
+	Start() error
+}
 
 func main() {
 	flag.Parse()
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	log.SetLevelByString(*level)
 
+	var s Server
+	var err error
+
 	switch *role {
-	case "instanc_agent":
-		cfg, err := util.GetAgentCfg(*cfgPath)
+	case "agent":
+		cfg, err := agent.GetCfg(*cfgPath)
 		if err != nil {
 			log.Fatal(err)
 		}
-		s, err := agent.NewInstanceAgent(cfg.Attr.DataDir, cfg.Attr.Ip, cfg.Attr.Port,
-			cfg.Attr.CtrlAddr)
-		if err == nil {
-			err = s.Start()
-		}
-		log.Fatal(err)
+		s, err = agent.NewAgent(cfg)
 	case "controller":
-		cfg, err := util.GetCtrlCfg(*cfgPath)
+		cfg, err := ctrl.GetCfg(*cfgPath)
 		if err != nil {
 			log.Fatal(err)
 		}
-		s := ctrl.NewController(cfg.Attr.DataDir, cfg.Attr.Addr)
-		log.Fatal(s.Start(cfg))
+		s, err = ctrl.NewController(cfg)
 	}
+
+	if err == nil {
+		err = s.Start()
+	}
+	log.Fatal(err)
 }
