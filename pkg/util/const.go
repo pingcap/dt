7b64a16ec76298b0122c3823d10afafdf8e33710
zimulala <zimu_xia@126.com>
2015-07-04 19:12:56 +0800

    rm the constants about url

--- pkg/util/const.go
+++ pkg/util/const.go
@@ -1,24 +1,5 @@
 package util
 
-// instance_agent api url
-const (
-	ActionStartInstance       = "api/start_instance"
-	ActionRestartInstance     = "api/restart_instance"
-	ActionPauseInstance       = "api/pause_instance"
-	ActionContinueInstance    = "api/continue_instance"
-	ActionStopInstance        = "api/stop_instance"
-	ActionDropPortInstance    = "api/dropport_instance"
-	ActionRecoverPortInstance = "api/Recoverport_instance"
-	ActionBackupInstanceData  = "api/backupdata_instance"
-	ActionCleanUpInstanceData = "api/cleanUpdata_instance"
-	ActionShutdown            = "api/Shutdown"
-)
-
-// ctroller api url
-const (
-	ActionRegisterAgent = "api/register_agent"
-)
-
 // ctroller test cmd name
 const (
 	TestCmdStart       = "start"
